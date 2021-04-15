4f1aa84ab635be9b9d2fe957c713345058c07542
morti-ru <info@morti.ru>
2021-04-15 16:54:39 +0700

    1.02

--- cmd/question.go
+++ cmd/question.go
@@ -25,18 +25,20 @@ import (
 var questionCmd = &cobra.Command{
 	Use:   "question",
 	Short: "Parameter for ask question",
-	Long: `Parameter for ask question big`,
+	Long:  `Parameter for ask question big`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Question asked:", args[0])
 		switch args[0] {
-			case "How are you?":
-				fmt.Println("I am fine")
-			case "Who am i?":
-				fmt.Println("I am batman!")
-			case "Good buy!":
-				fmt.Println("See you later!")
-			default:
-				fmt.Println("I have no answere!")
+		case "How are you?":
+			fmt.Println("I am fine")
+		case "Who am i?":
+			fmt.Println("I am batman!")
+		case "Good buy!":
+			fmt.Println("See you later!")
+		case "Time?":
+			fmt.Println("I don t now!")
+		default:
+			fmt.Println("I have no answere!")
 		}
 	},
 }
